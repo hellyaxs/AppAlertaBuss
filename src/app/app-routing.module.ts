882f94801dbeb83bcb98453d445e8fc9eda89bc1
hellyaxs <eliasvitor.dev@gmail.com>
2022-04-14 17:22:06 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componemts/home/home.component';
+import { LoginComponent } from './componemts/Login/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component:LoginComponent ,pathMatch:"full"},
+  {path:"home", component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/componemts/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+   x:Boolean = true;
+   nome:string = "" ;
+
+  constructor(){ }
+
+  ngOnInit(): void {
+  }
+
+  alterar():void{
+    this.x = !this.x;
+  }
+
+}
